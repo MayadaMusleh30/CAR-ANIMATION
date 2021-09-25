d973a6bbc46efaa23e5922f70f406036035cb293
Mayada Musleh <41662966+MayadaMusleh30@users.noreply.github.com>
2021-09-25 13:12:48 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Variables
+    $surface = $('.surface');
+    $car = $('.car');
+
+    // keypress event
+    $(document).on('keypress', function(e){
+        if(e.which == 13){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+        }
+
+    })
+});
